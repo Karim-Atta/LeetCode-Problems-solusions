38aeaff36fe5bcf97e95624611a299df7c8fc53a
karem-Ata1 <81815772+karem-Ata1@users.noreply.github.com>
2022-07-02 03:40:15 +0200

    Time: 72 ms (23.04%), Space: 56.7 MB (23.69%) - LeetHub

---
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String reverseWords(String s) {
+        Stack stack = new Stack();
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i=0; i<s.length(); i++){
+            if(s.charAt(i) == ' '){
+                while(!stack.empty()){
+                    sb.append(stack.pop());
+                }  
+                sb.append(' ');
+            }
+            else{
+                stack.push(s.charAt(i));
+            }
+        }
+        while(!stack.empty()){
+            sb.append(stack.pop());
+        }
+        return sb.toString();
+    }
+}
