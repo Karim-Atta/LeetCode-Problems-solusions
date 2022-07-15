daa1f9bd50c0214478d6f5483cbe888bc3e03868
karem-Ata1 <81815772+karem-Ata1@users.noreply.github.com>
2022-07-15 22:49:34 +0200

    Time: 28 ms (56.80%), Space: 51.8 MB (24.91%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,19 +1,31 @@
-class Solution {
-    int[] numbers;
-    public int findTargetSumWays(int[] nums, int target) {
-        numbers = nums;
-        return dfs(0, target, 0);
+public class Solution {
+    int total;
+    
+    public int findTargetSumWays(int[] nums, int S) {
+        total = Arrays.stream(nums).sum();
+        
+        int[][] memo = new int[nums.length][2* total + 1];
+        for (int[] row : memo) {
+            Arrays.fill(row, Integer.MIN_VALUE);
+        }
+        return calculate(nums, 0, 0, S, memo);
     }
     
-    public int dfs(int index, int target, int result){
-        if(index == numbers.length){
-            if(result == target)
+    public int calculate(int[] nums, int i, int sum, int S, int[][] memo) {
+        if (i == nums.length) {
+            if (sum == S) {
                 return 1;
-            return 0;
+            } else {
+                return 0;
+            }
+        } else {
+            if (memo[i][sum + total] != Integer.MIN_VALUE) {
+                return memo[i][sum + total];
+            }
+            int add = calculate(nums, i + 1, sum + nums[i], S, memo);
+            int subtract = calculate(nums, i + 1, sum - nums[i], S, memo);
+            memo[i][sum + total] = add + subtract;
+            return memo[i][sum + total];
         }
-        
-        int negative = dfs(index + 1, target,  result + numbers[index] * -1);
-        int positive = dfs(index + 1, target, result + numbers[index]);
-        return negative + positive;
     }
 }
