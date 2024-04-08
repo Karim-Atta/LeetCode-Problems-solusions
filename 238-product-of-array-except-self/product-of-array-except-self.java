cd12b4eeec14c6f1ea7dc59b24dba2e85dc6ea8c
Karim-Atta <81815772+Karim-Atta@users.noreply.github.com>
2024-04-09 00:01:15 +0200

    Time: 2 ms (76.40%) | Memory: 55.1 MB (13.89%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,30 +1,18 @@
 class Solution {
-    public int[] productExceptSelf(int[] nums) {
-
-        int[] answer = new int[nums.length]; 
-
-        int[] productofTheFollowingNum = new int[nums.length];
-        int[] productOfThePreviousNum = new int[nums.length];
-
-        int currentPrevious = 1;
-        int currentFollowing = 1;
-
-        productOfThePreviousNum[0] = 1;
-        for (int i = 1; i < nums.length; i++){
-            productOfThePreviousNum[i] = currentPrevious * nums[i - 1];
-            currentPrevious = productOfThePreviousNum[i];
+   public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int ans[] = new int[n];
+        Arrays.fill(ans, 1);
+        int curr = 1;
+        for(int i = 0; i < n; i++) {
+            ans[i] *= curr;
+            curr *= nums[i];
         }
-
-        productofTheFollowingNum[nums.length - 1] = 1;
-        for (int i = nums.length - 2; i >= 0; i--){
-            productofTheFollowingNum[i] = currentFollowing * nums[i + 1];
-            currentFollowing = productofTheFollowingNum[i];
+        curr = 1;
+        for(int i = n - 1; i >= 0; i--) {
+            ans[i] *= curr;
+            curr *= nums[i];
         }
-
-        for(int i = 0; i < nums.length; i++){
-            answer[i] = productofTheFollowingNum[i] * productOfThePreviousNum[i];
-        }
-        
-        return answer;
+        return ans;
     }
 }
